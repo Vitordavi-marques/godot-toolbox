bcbd118b0b9434557a27a1cf9da692002299f373
vitor.marques <vitor.marques@gatec.com.br>
2021-07-15 17:43:34 -0300

    GTInputController added to the toolbox

--- libs/gt_input_controller/__gt_input_controller.gd
+++ libs/gt_input_controller/__gt_input_controller.gd
@@ -4,6 +4,7 @@ class_name GTInputController, "res://libs/gt_input_controller/icons/gt_default_c
 enum { JUST_PRESSED, PRESSED, RELEASED }
 const METHODS = [ JUST_PRESSED, PRESSED, RELEASED ]
 
+export (String) var tag = ""
 export (bool) var is_active = true
 export (Array, String) var actions
 
@@ -13,11 +14,26 @@ func _ready():
 	for action in actions:
 		values[action] = { JUST_PRESSED: false, PRESSED: false, RELEASED: false }
 
+# Sets all values for each action and method to false
 func clear_input():
-	pass
+	for action in actions:
+		for method in METHODS:
+			set_input(action, method, false)
 
+# Returns the value for the given action and method
+func get_input(action: String, method: int):
+	return values[get_action(action)][method]
+
+# Sets the given value for this action and method
+func set_input(action: String, method: int, value):
+	values[get_action(action)][method] = value
+
+# Returns the given action plus this input controller's tag as a suffix
+func get_action(action: String):
+	if not tag.empty():
+		return "%s_%s" % [action, tag]
+	return action
+
+# Captures the input state of the controller's actions in the current moment
 func poll_input():
-	pass
-
-func get_input(action, method):
 	pass
