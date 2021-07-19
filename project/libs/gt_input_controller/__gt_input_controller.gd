30d181d9ea765b11c2cddc5adec0ea028809b8f3
vitor.marques <vitor.marques@gatec.com.br>
2021-07-18 22:45:54 -0300

    GTInputController updates

--- project/libs/gt_input_controller/__gt_input_controller.gd
+++ project/libs/gt_input_controller/__gt_input_controller.gd
@@ -1,8 +1,8 @@
 extends Node
 class_name GTInputController, "res://libs/gt_input_controller/icons/gt_default_controller.svg"
 
-enum { JUST_PRESSED, PRESSED, RELEASED }
-const METHODS = [ JUST_PRESSED, PRESSED, RELEASED ]
+enum { JUST_PRESSED = 0, PRESSED = 1, JUST_RELEASED = 2 }
+const METHODS = [ JUST_PRESSED, PRESSED, JUST_RELEASED ]
 
 export (String) var tag = ""
 export (bool) var is_active = true
@@ -12,28 +12,33 @@ var values = {}
 
 func _ready():
 	for action in actions:
-		values[action] = { JUST_PRESSED: false, PRESSED: false, RELEASED: false }
+		values[action] = { JUST_PRESSED: false, PRESSED: false, JUST_RELEASED: false }
 
-# Sets all values for each action and method to false
-func clear_input():
+# Resets all values for each action and method to false
+func clear_input() -> void:
 	for action in actions:
 		for method in METHODS:
 			set_input(action, method, false)
 
 # Returns the value for the given action and method
-func get_input(action: String, method: int):
+func get_input(action: String, method: int) -> bool:
 	return values[get_action(action)][method]
 
 # Sets the given value for this action and method
-func set_input(action: String, method: int, value):
+func set_input(action: String, method: int, value: bool) -> void:
 	values[get_action(action)][method] = value
 
 # Returns the given action plus this input controller's tag as a suffix
-func get_action(action: String):
+func get_action(action: String) -> String:
 	if not tag.empty():
 		return "%s_%s" % [action, tag]
 	return action
 
-# Captures the input state of the controller's actions in the current moment
-func poll_input():
+# Captures the input state of the controller's actions in the current moment, if enabled
+func poll_input() -> void:
+	if is_active:
+		poll_state()
+
+# Implementation of the method that polls input state
+func poll_state() -> void:
 	pass
