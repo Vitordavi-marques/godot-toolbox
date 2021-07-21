aeeffa65c47777e38e2f7917b03619866d6486e2
vitor.marques <vitor.marques@gatec.com.br>
2021-07-21 18:22:00 -0300

    Changes to GTRandomPointZone2D

--- libs/gt_random_point_zone_2d/gt_random_point_zone_2d.gd
+++ libs/gt_random_point_zone_2d/gt_random_point_zone_2d.gd
@@ -19,20 +19,20 @@ func random_point() -> Vector2:
 		return random_point_circle(rand_shape)
 	return Vector2.ZERO
 
-func random_point_rectangle(rand_shape: CollisionShape2D) -> Vector2:
-	var size = 2*rand_shape.shape.extents
+func random_point_rectangle(shape: CollisionShape2D) -> Vector2:
+	var size = 2*shape.shape.extents
 	
-	var rand_pos_x = posmod(randi(),size.x) - (size.x/2) + rand_shape.global_position.x
-	var rand_pos_y = posmod(randi(),size.y) - (size.y/2) + rand_shape.global_position.y
+	var rand_pos_x = posmod(randi(),size.x) - (size.x/2) + shape.global_position.x
+	var rand_pos_y = posmod(randi(),size.y) - (size.y/2) + shape.global_position.y
 	var rand_pos = Vector2(rand_pos_x, rand_pos_y)
 	return rand_pos
 
-func random_point_circle(rand_shape: CollisionShape2D) -> Vector2:
-	var radius = rand_shape.shape.radius
+func random_point_circle(shape: CollisionShape2D) -> Vector2:
+	var radius = shape.shape.radius
 	var rand_r = radius * sqrt(randf())
 	var rand_theta = 2*PI * randf()
 	
-	var rand_pos_x = rand_shape.x + rand_r*cos(rand_theta)
-	var rand_pos_y = rand_shape.y + rand_r*sin(rand_theta)
+	var rand_pos_x = shape.x + rand_r*cos(rand_theta)
+	var rand_pos_y = shape.y + rand_r*sin(rand_theta)
 	var rand_pos = Vector2(rand_pos_x, rand_pos_y)
 	return rand_pos
