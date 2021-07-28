9a47bb34f810effe147cf4909dda0d6797d0ab5a
vitor.marques <vitor.marques@gatec.com.br>
2021-07-27 21:26:26 -0300

    Fixed a bug in GTTriggerArea2D

--- libs/gt_area_2d/gt_trigger_area_2d/gt_trigger_area_2d.gd
+++ libs/gt_area_2d/gt_trigger_area_2d/gt_trigger_area_2d.gd
@@ -8,10 +8,10 @@ enum TRIGGER_TYPE { ENTER = 0, INSIDE = 1, EXIT = 2 }
 export (TRIGGER_TYPE) var trigger_type = TRIGGER_TYPE.ENTER
 
 func _ready():
-	connect("area_entered", self, "_on_grouped_area_entered")
-	connect("area_exited", self, "_on_grouped_area_exited")
-	connect("body_entered", self, "_on_grouped_body_entered")
-	connect("body_exited", self, "_on_grouped_body_exited")
+	connect("grouped_area_entered", self, "_on_grouped_area_entered")
+	connect("grouped_area_exited", self, "_on_grouped_area_exited")
+	connect("grouped_body_entered", self, "_on_grouped_body_entered")
+	connect("grouped_body_exited", self, "_on_grouped_body_exited")
 
 func _physics_process(delta):
 	if trigger_type == TRIGGER_TYPE.INSIDE:
